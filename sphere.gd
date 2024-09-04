7c56dcec80b27a7690d16bf73a3ebdda9292e407
chill <chill@mail.com>
2024-09-05 00:34:44 +0300

    Move by raycast works

--- sphere.gd
+++ sphere.gd
@@ -7,20 +7,21 @@ var collided: bool = false
 func _process(delta: float) -> void:
 	if !collided:
 		return
-	%ProgressBar.value += delta*3
+	#%ProgressBar.value += delta*3
 		
-	if Input.is_action_pressed("KEY_W"):
-		position -= global_basis.z * speed * delta
-	elif Input.is_action_pressed("KEY_S"):
-		position += global_basis.z * speed * delta
-	elif Input.is_action_pressed("KEY_A"):
-		position -= global_basis.x * speed * delta
-	elif Input.is_action_pressed("KEY_D"):
-		position += global_basis.x * speed * delta
+	#if Input.is_action_pressed("KEY_W"):
+		#position -= global_basis.z * speed * delta
+	#elif Input.is_action_pressed("KEY_S"):
+		#position += global_basis.z * speed * delta
+	#elif Input.is_action_pressed("KEY_A"):
+		#position -= global_basis.x * speed * delta
+	#elif Input.is_action_pressed("KEY_D"):
+		#position += global_basis.x * speed * delta
 		
 	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_length = 100
 	var from = %Camera3D.project_ray_origin(mouse_pos)
-	var to = from + %Camera3D.project_ray_normal(mouse_pos) * 500
+	var to = from + %Camera3D.project_ray_normal(mouse_pos) * ray_length
 	var space = get_world_3d().direct_space_state
 	var ray_query = PhysicsRayQueryParameters3D.new()
 	ray_query.from = from
@@ -28,7 +29,11 @@ func _process(delta: float) -> void:
 	ray_query.collide_with_areas = true
 	var raycast_result = space.intersect_ray(ray_query)
 	
-	print(raycast_result)
+	if raycast_result.size() > 0:
+		var pos = raycast_result.position
+		pos.y = position.y
+		var dir = (pos - position).normalized()
+		position += dir*delta
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
