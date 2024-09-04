74c97408295a15955e7a6b637ab7bc9eac2cfe3e
chill <chill@mail.com>
2024-09-04 16:28:46 +0300

    If else works

--- sphere.gd
+++ sphere.gd
@@ -3,6 +3,13 @@ extends RigidBody3D
 
 #func _ready() -> void:
 	#pass
+var collided: bool = false
 
 func _process(delta: float) -> void:
-	linear_velocity = transform.basis.z * 2;
+	if !collided:
+		return
+	linear_velocity = transform.basis.z * 2
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	collided = true
