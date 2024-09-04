8f4ec0466468d0eed2ed66185ebc9d392370975b
chill <chill@mail.com>
2024-09-04 17:41:52 +0300

    Add movement with keyboard

--- sphere.gd
+++ sphere.gd
@@ -1,17 +1,21 @@
 extends RigidBody3D
 
 
-#func _ready() -> void:
-	#pass
-	
 @export var speed: float
-
 var collided: bool = false
-
+		
 func _process(delta: float) -> void:
 	if !collided:
 		return
-	position += basis.z*speed*delta
+		
+	if Input.is_action_pressed("KEY_W"):
+		position -= global_basis.z * speed * delta
+	elif Input.is_action_pressed("KEY_S"):
+		position += global_basis.z * speed * delta
+	elif Input.is_action_pressed("KEY_A"):
+		position -= global_basis.x * speed * delta
+	elif Input.is_action_pressed("KEY_D"):
+		position += global_basis.x * speed * delta
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
