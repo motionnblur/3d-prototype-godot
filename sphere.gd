0c4423fd06aea1916fbefdffc7b0e6e85f35d392
chill <chill@mail.com>
2024-09-04 17:12:34 +0300

    Update movement

--- sphere.gd
+++ sphere.gd
@@ -3,12 +3,15 @@ extends RigidBody3D
 
 #func _ready() -> void:
 	#pass
+	
+@export var speed: float
+
 var collided: bool = false
 
 func _process(delta: float) -> void:
 	if !collided:
 		return
-	linear_velocity = transform.basis.z * 2
+	position += basis.z*speed*delta
 
 
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
