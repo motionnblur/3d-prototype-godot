6193029851a1d9fff8c6449a18104de7802675a4
chill <chill@mail.com>
2024-09-09 23:51:35 +0300

    Add win code

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -6,6 +6,9 @@ var current_step: int = 0
 var label: Label
 var label2: Label
 var is_end: bool = false
+var is_win: bool = false
+var goals: int
+var goal_counter: int = 0
 
 func _ready() -> void:
 	current_step = max_step
@@ -22,9 +25,14 @@ func addStep() -> void:
 	else:
 		label.text = "0"	
 		is_end = true
+
+func increaseGoalCount() -> void:
+	goal_counter += 1
 		
 func end() -> void:
-	if is_end:
+	if goal_counter == goals:
+		print("win")
+	elif is_end:
 		await Global.delay(0.7)
 		label2.show()
 		await Global.delay(1.5)
--- scripts/score.gd
+++ scripts/score.gd
@@ -1,13 +1,16 @@
 extends Node
 
+var level_manager
 var progress_bar: ProgressBar
 var audio: AudioStreamPlayer
 
 func _ready() -> void:
 	progress_bar = get_node("/root/Node3D/UI/ProgressBar")
 	audio = get_node("/root/Node3D/Audio")
+	level_manager = get_node("/root/Node3D/LevelManager")
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	area.queue_free()
 	progress_bar.value += 5
 	audio.play()
+	level_manager.increaseGoalCount()
