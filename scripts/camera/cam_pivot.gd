26cc391ace01ab0ea4fb94ffc96fb36e60ff19af
chill <chill@mail.com>
2024-09-11 02:07:31 +0300

    Fix camera rotating (rotate /w drag)

--- scripts/camera/cam_pivot.gd
+++ scripts/camera/cam_pivot.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 var isRightClick: bool = false
 var deltaMouse: Vector2
+var mousePos1: Vector2
+var mousePos2: Vector2
+var posPhase: int = 1
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Right_Click"):
@@ -14,6 +17,7 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	if !isRightClick: return
+	if !isDragging(): return
 	
 	var xRot = rotation_degrees.x - deltaMouse.y * delta * 8
 	var yRot = rotation_degrees.y - deltaMouse.x * delta * 8
@@ -22,3 +26,17 @@ func _process(delta: float) -> void:
 	#yRot = clamp(yRot, 0, 90)
 	
 	rotation_degrees = Vector3(xRot, yRot, rotation_degrees.z)
+
+func isDragging() -> bool:
+	var m_pos = get_viewport().get_mouse_position()
+	if posPhase == 1:
+		mousePos1 = m_pos
+		posPhase = 2
+	elif posPhase == 2:
+		mousePos2 = m_pos
+		posPhase = 1
+	
+	if mousePos1 == mousePos2:
+		return false
+	else:
+		return true
