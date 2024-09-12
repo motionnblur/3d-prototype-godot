62ee87afea9d325375d9195b01999f16854995a1
chill <chill@mail.com>
2024-09-12 04:02:17 +0300

    Small refactor

--- scripts/camera/lookat.gd
+++ scripts/camera/lookat.gd
@@ -7,5 +7,5 @@ func _ready() -> void:
 	cam = get_node("/root/Node3D/CamPivot/Camera3D")
 	terrain = get_node("/root/Node3D/Terrain")
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	cam.look_at(terrain.transform.origin)
